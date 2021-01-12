00ba601f4fdfb772d6afc2a13fa26104a695d5a0
quintus <quzhu@MBP2020.local>
2021-01-13 03:42:06 +0800

    intolerableness bug for ssrp preview, revert to release version

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -28,10 +28,10 @@ svn co https://github.com/project-openwrt/openwrt/branches/master/package/ctcgfw
 git clone https://github.com/natelol/luci-app-oled package/natelol/luci-app-oled
 
 #SSRP
-#svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 #svn co https://github.com/fw876/helloworld/trunk/tcping package/lean/tcping
 #svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/lean/naiveproxy
-svn co https://github.com/Mattraks/helloworld/branches/Preview/luci-app-ssr-plus package/lean/luci-app-ssr-plus
+#svn co https://github.com/Mattraks/helloworld/branches/Preview/luci-app-ssr-plus package/lean/luci-app-ssr-plus
 svn co https://github.com/Mattraks/helloworld/branches/Preview/naiveproxy package/lean/naiveproxy
 svn co https://github.com/Mattraks/helloworld/branches/Preview/tcping package/lean/tcping
 
